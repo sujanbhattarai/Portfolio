6ab7a77a0b41f1471ebd311dfe096c5b09e32aa1
sujan bhattarai <sujanbhattarai01@gmail.com>
2020-11-24 00:58:04 -0600

    Updated file

---
+++ build/precache-manifest.1f33a703859335c97327f489d657456d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d599df971476660de76e674679f276",
+    "url": "/index.html"
+  },
+  {
+    "revision": "799b254fb226342f4128",
+    "url": "/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "5badc5c8e62ecd5ffdfa",
+    "url": "/static/css/main.5f5e225f.chunk.css"
+  },
+  {
+    "revision": "799b254fb226342f4128",
+    "url": "/static/js/2.bc976612.chunk.js"
+  },
+  {
+    "revision": "8e5ae93e18f66c1d607de05f5b5d011b",
+    "url": "/static/js/2.bc976612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5badc5c8e62ecd5ffdfa",
+    "url": "/static/js/main.cb3fb92e.chunk.js"
+  },
+  {
+    "revision": "12576549ede2f88a6b13",
+    "url": "/static/js/runtime-main.fa64e08d.js"
+  }
+]);
